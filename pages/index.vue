94c0d1ca6c0debe9a618dddf05631d36cdbfa5c4
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-07 19:28:39 +0300

    First try

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="block green"></div>
+    <div class="block black"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$gsap.to(document.querySelector('.green'), { ease:'bounce', x: 1000, duration: 5, marginBottom: 10,})
+    this.$gsap.to(document.querySelector('.black'), { ease:'bounce', x: 1000, duration: 5})
+
+  },
+}
+</script>
+<style lang="scss" scoped>
+.block {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+
+  &.green {
+    background-color: green;
+  }
+
+  &.black {
+    background-color: black;
+  }
+}
+</style>
